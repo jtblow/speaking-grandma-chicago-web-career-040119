f082491bfafc06ac35daca5e6d079ec358b6901b
creative-zombie-4458 <jtbartlow@gmail.com>
2019-03-18 21:09:13 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -13,10 +13,10 @@
 # 'I LOVE YOU TOO PUMPKIN!'
 def speak_to_grandma(phrase)
   if phrase == "I LOVE YOU GRANDMA!"
-   print "I LOVE YOU TOO PUMPKIN!"
+   "I LOVE YOU TOO PUMPKIN!"
    elsif phrase ! phrase.upcase
-   print "HUH! SPEAK UP, SONNY!"
+   "HUH! SPEAK UP, SONNY!"
     else 
-    print "NO, NOT SINCE 1938!"
+    "NO, NOT SINCE 1938!"
   end
 end
